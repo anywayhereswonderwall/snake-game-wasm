4ea2b1c059df24e35e7fde5aa665653f11902fef
Michal Kaliszewski <michal.kaliszewski2@gmail.com>
2022-09-13 02:50:53 +0200

    snake self collision detection

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
+use std::collections::HashSet;
+
 use rand::Rng;
 use wasm_bindgen::prelude::*;
+
+// RETURNS RANDON NUMBER IN GIVEN RANGE
+pub fn random_range(min: usize, max: usize) -> usize {
+    let mut rng = rand::thread_rng();
+    let value = rng.gen_range(min..max);
+    value
+}
+// CHECKS FOR DUPLICATES IN A VECTOR
+pub fn contains_duplicates(vec: &Vec<usize>) -> bool {
+    let mut values = HashSet::new();
+    for e in vec {
+        values.insert(e);
+    };
+    !(values.len() == vec.len())
+}
+
 
 #[wasm_bindgen]
 pub enum Direction {
@@ -8,14 +26,6 @@ pub enum Direction {
     Down,
     Left
 }
-// pub type Position = (usize, usize);
-
-pub fn random_range(min: usize, max: usize) -> usize {
-    let mut rng = rand::thread_rng();
-    let value = rng.gen_range(min..max);
-    value
-}
-
 #[wasm_bindgen]
 pub struct SnakeGame {
     width: usize,
@@ -29,16 +39,18 @@ pub struct SnakeGame {
 
 #[wasm_bindgen]
 impl SnakeGame {
+    // CONSTRUCTOR
     pub fn new(width: usize, height: usize) -> Self {
         Self { 
             width, 
             height, 
-            snake: vec![20, 21, 22],
+            snake: vec![17, 18, 19, 20, 21],
             direction: Direction::Left,
             food: random_range(0, width * height),
             lost: false
         }
     }
+    // HANDLES USER'S DIRECTIONS INPUT
     pub fn change_direction(&mut self, direction: Direction) {
         match (&self.direction, direction) {
         (Direction::Up, Direction::Right) => self.direction = Direction::Right,
@@ -52,7 +64,9 @@ impl SnakeGame {
         _ => {}
         }
     }
+    
     pub fn tick(&mut self) {
+        self.snake_self_collision();
         if self.lost {
             return;
         }
@@ -69,9 +83,17 @@ impl SnakeGame {
             self.generate_food();
         }
     }
+    pub fn snake_self_collision(&mut self) {
+        if contains_duplicates(&self.snake) {
+            self.lost = true;
+        }
+    }
+
+    // RANDOMLY PLACES FOOD ON THE BOARD
     pub fn generate_food(&mut self) {
         self.food = random_range(0, self.width * self.height);
-    } 
+    }
+    // GETTER FUNCTIONS FOR JS
     pub fn snake_body(&self) -> *const usize {
         self.snake.as_ptr()
     }
